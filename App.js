77c77f9aeac53276a78544e1c70f182cc58b2ac3
Hannah <vining.h@husky.neu.edu>
2024-04-11 21:22:55 -0400

    app js edits

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import AssignmentRoutes from './Kanbas/assignments/routes.js';
 import cors from "cors";
 import "dotenv/config";
 import session from "express-session";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors(
     {
